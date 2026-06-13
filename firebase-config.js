d4e05d0031d7f3137fb577bfbd62e2d86ef77418
Jose Francisco <josefranciisco17@gmail.com>
2026-06-12 18:34:28 -0600

    Actualización sistema MIDI

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
-import {
-getAuth,
-GoogleAuthProvider
-} from "https://www.gstatic.com/firebasejs/12.14.0/firebase-auth.js";
-
-import {
-getFirestore
-} from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-storage.js";
 
 const firebaseConfig = {
 apiKey: "AIzaSyArKUqthRHDrO1xXn39wJP-JCiBZdilOcY",
@@ -19,6 +13,5 @@ appId: "1:79307845171:web:c21918694f6d916fbbfd46"
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
+export const storage = getStorage(app);
